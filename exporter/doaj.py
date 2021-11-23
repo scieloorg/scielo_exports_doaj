c243e59308d4c61f5ff6bf6f5f006e1a7b36476b
patymori <excermori@yahoo.com.br>
2021-11-23 15:05:10 -0300

    Adiciona campo identifier no DOAJDocument

--- exporter/doaj.py
+++ exporter/doaj.py
@@ -9,15 +9,24 @@ class DOAJDocumentNoAuthorsException(Exception):
     pass
 
 
+class DOAJDocumentNoISSNException(Exception):
+    pass
+
+
 class DOAJDocument(interfaces.ExporterInterface):
     def __init__(self, article: scielodocument.Article):
         self.data = {}
         self.data.setdefault("bibjson", {})
         self.add_bibjson_author(article)
+        self.add_bibjson_identifier(article)
 
     @property
     def bibjson_author(self) -> typing.List[dict]:
         return self.data["bibjson"]["author"]
+
+    @property
+    def bibjson_identifier(self) -> typing.List[dict]:
+        return self.data["bibjson"]["identifier"]
 
     def add_bibjson_author(self, article: scielodocument.Article):
         if not article.data.authors:
@@ -27,6 +36,23 @@ class DOAJDocument(interfaces.ExporterInterface):
         for author in article.data.authors:
             author_name = [author.get('given_names', ''), author.get('surname', '')]
             self.data["bibjson"]["author"].append({"name": author_name})
+
+    def add_bibjson_identifier(self, article: scielodocument.Article):
+        issn = article.data.journal.any_issn()
+        if not issn:
+            raise DOAJDocumentNoISSNException()
+
+        if issn == article.data.journal.electronic_issn:
+            issn_type = "eissn"
+        else:
+            issn_type = "pissn"
+
+        self.data["bibjson"]["identifier"] = [{"id": issn, "type": issn_type}]
+
+        if article.data.doi:
+            self.data["bibjson"]["identifier"].append(
+                {"id": article.data.doi, "type": "doi"}
+            )
 
     def get_request(self):
         return {}
--- tests/test_doaj.py
+++ tests/test_doaj.py
@@ -23,6 +23,22 @@ class DOAJDocumentTest(TestCase):
                     self.doaj_document.bibjson_author,
                 )
 
+    def test_bibjson_identifier(self):
+        issn = self.article.data.journal.any_issn()
+        if issn == self.article.data.journal.electronic_issn:
+            issn_type = "eissn"
+        else:
+            issn_type = "pissn"
+
+        self.assertIn(
+            {"id": issn, "type": issn_type},
+            self.doaj_document.bibjson_identifier,
+        )
+        self.assertIn(
+            {"id": self.article.data.doi, "type": "doi"},
+            self.doaj_document.bibjson_identifier,
+        )
+
 
 class DOAJDocumentErrorsTest(TestCase):
     @vcr.use_cassette("tests/fixtures/vcr_cassettes/S0100-19651998000200002.yml")
@@ -34,4 +50,10 @@ class DOAJDocumentErrorsTest(TestCase):
     def test_raises_exception_if_no_author(self):
         del self.article.data.data["article"]["v10"]    # v10: authors
         with self.assertRaises(doaj.DOAJDocumentNoAuthorsException) as exc:
+            doaj.DOAJDocument(article=self.article)
+
+    def test_raises_exception_if_no_eissn_nor_pissn(self):
+        self.article.data.journal.electronic_issn = None
+        self.article.data.journal.print_issn = None
+        with self.assertRaises(doaj.DOAJDocumentNoISSNException) as exc:
             doaj.DOAJDocument(article=self.article)
