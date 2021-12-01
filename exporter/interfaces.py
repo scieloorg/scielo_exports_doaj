532ab25f475f16bfa76c8a322d1a00c6c6d6724d
patymori <excermori@yahoo.com.br>
2021-11-30 23:26:01 -0300

    Adiciona post_request à interface de exportador e às implementações
    Requisição para POST /articles

--- exporter/interfaces.py
+++ exporter/interfaces.py
@@ -3,5 +3,9 @@ from abc import ABC, abstractmethod
 
 class IndexExporterInterface(ABC):
     @abstractmethod
+    def post_request(self):
+        pass
+
+    @abstractmethod
     def export(self):
         pass
